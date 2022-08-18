3bd0f5caa17e5af19fb63465f0f09d203bc3fb31
Sashko Milushev <milushev.sashko@gmail.com>
2022-08-18 12:11:34 +0300

    Implement the subclasses of 'CookedDish'.

---
+++ dish_structure/cooked_dish/main_dish.py
@@ -0,0 +1,6 @@
+from dish_structure.cooked_dish import CookedDish
+
+
+class MainDish(CookedDish):
+    def __init__(self, name: str, weight: int, ingredients: list, price: float):
+        super().__init__(name, weight, ingredients, price)
