dc66f2627b8b6ae6df27392fbd04b65ac20c4693
Sashko Milushev <milushev.sashko@gmail.com>
2022-08-19 16:37:22 +0300

    Implement login functionality.

--- user_interface/auth_service.py
+++ user_interface/auth_service.py
@@ -1,5 +1,7 @@
 import json
 
+USERS_FILE_PATH = './db/users.txt'
+
 
 def register(username, password):
     user_object = {
@@ -8,10 +10,19 @@ def register(username, password):
     }
 
     user_json = json.dumps(user_object)
-    with open('./db/users.txt', 'r+') as file:
+    with open(USERS_FILE_PATH, 'r+') as file:
         for user_line in file:
             existing_user = json.loads(user_line.strip())
             if existing_user['username'] == username:
                 return False
         file.write(user_json + '\n')
         return True
+
+
+def login(username, password):
+    with open(USERS_FILE_PATH, 'r') as file:
+        for user_line in file:
+            existing_user = json.loads(user_line.strip())
+            if existing_user['username'] == username and existing_user['password'] == password:
+                return True
+        return False
--- user_interface/main.py
+++ user_interface/main.py
@@ -1,11 +1,35 @@
 import tkinter as tk
 
-from user_interface.auth_service import register
+from user_interface.auth_service import register, login
 
 
 def clear_window(window):
     for child in window.winfo_children():
         child.destroy()
+
+
+def render_login_screen(window):
+    clear_window(window)
+
+    tk.Label(window, text='Enter your username: ').grid(row=0, column=0)
+    username = tk.Entry(window)
+    username.grid(row=0, column=1)
+
+    tk.Label(window, text='Enter your password: ').grid(row=1, column=0)
+    password = tk.Entry(window, show='*')
+    password.grid(row=1, column=1)
+
+    def on_login():
+        username_value = username.get()
+        password_value = password.get()
+
+        result = login(username_value, password_value)
+        if result:
+            render_home_screen(window)
+        else:
+            tk.Label(window, text='Invalid credentials', fg='red').grid(row=2, column=1)
+
+    tk.Button(window, text='Login', bg='orange', fg='white', command=on_login).grid(row=3, column=1)
 
 
 def render_main_screen(window):
@@ -44,6 +68,9 @@ def render_register_screem(window):
                 tk.Label(window, text='This username is already used!', fg='red').grid(row=3, column=1)
 
     tk.Button(window, text='Submit', bg='green', fg='yellow', command=on_register).grid(row=4, column=1)
+
+def render_home_screen(window):
+    clear_window(window)
 
 
 
